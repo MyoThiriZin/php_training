1a4e76421261c49b4c143e735cb1b69546a34301
MyoThiriZin <myothirizin99@gmail.com>
2022-02-16 12:14:43 +0630

    add tutorial

---
+++ tutorial_1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html> 
+    <body> 
+        <h1>Chess Board</h1>
+        <table width="320px" cellspacing="0px" cellpadding="0px" border="1px">
+            <?php
+            for ($row = 1; $row <= 8; $row++) {
+                echo "<tr>";
+                for ($col = 1; $col <= 8; $col++) {
+                    $sum = $row + $col;
+                    if ($sum % 2 == 0) {
+                        echo "<td height=40px width=40px bgcolor=#FFFFFF></td>";
+                    } else {
+                        echo "<td height=40px width=40px bgcolor=#000000></td>";
+                    }
+                }
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </body>
+</html>
