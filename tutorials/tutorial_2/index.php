18ef8a7619a42c089acaed5ff5dc141adeab9252
MyoThiriZin <myothirizin99@gmail.com>
2022-02-17 15:53:52 +0630

    [master]tutorial_1 and tutorial_2 space bar edited

--- tutorials/tutorial_2/index.php
+++ tutorials/tutorial_2/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Star</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Star</title>
 </head>
 <body>
     <?php
@@ -26,8 +26,8 @@
             } else {
                 echo "&nbsp;&nbsp;&nbsp;";
             }
-          }
-          echo "<br/>";
+        }
+        echo "<br/>";
     }
     ?> 
 </body>
